0241a6f9570ae9327c0d034826fb37275b962a1a
Noah Husain <noahhusain@gmail.com>
2021-05-20 11:13:13 -0500

    Added save and cancel buttons to new post section

--- src/scripts/feed/CreatePost.js
+++ src/scripts/feed/CreatePost.js
@@ -1,19 +1,23 @@
 export const newPostButton = () => {
-    return `<button class="button" id="create__button">Create Post</button>`
+    return `<button class="miniMode" id="create__button">Have a gif to post?</button>`
 }
 
 export const CreatePost = () => {
     return `
     <div class="field">
-        <input placeholder ="Title" type="text" name="title" class="input"/>
+        <input placeholder ="Title" type="text" name="title" class="newPost__input"/>
     </div>
 
     <div class="field">
-        <input placeholder ="URL" type="text" name="url" class="input"/>
+        <input placeholder ="URL" type="text" name="url" class="newPost__input"/>
     </div>
 
     <div class="field">
-        <input placeholder ="Story behind your gif..." type="text" name="story" class="input"/>
+        <input placeholder ="Story behind your gif..." type="text" name="story" class="newPost__input"/>
     </div>
+
+    <button class="newPost__submit" id="create__button">Save</button>
+    <button class="newPost__cancel" id="create__button">Cancel</button>
+    
     `
 }
