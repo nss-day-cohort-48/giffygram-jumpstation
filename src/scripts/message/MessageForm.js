19b0ea6106c51354ac9925a458cf396f5b82e3bd
shawnbarron16 <shawnbarron16@gmail.com>
2021-05-20 11:51:33 -0500

    Saving MessageForm

---
+++ src/scripts/message/MessageForm.js
@@ -0,0 +1,5 @@
+const messageForm =  () => {
+    let html = /*html*/ `
+    Message<br><br><input type="text" placeholder="Message to user"><br>
+    <input type="Button" value="Save" name="save"><input type="Button" value="Cancel" name="cancel">`
+}
