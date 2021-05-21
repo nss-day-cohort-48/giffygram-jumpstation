b35cb1abdaadb22df4e73e1c3755cfa1ee29cad9
shawnbarron16 <shawnbarron16@gmail.com>
2021-05-21 11:06:55 -0500

    render message form

--- src/scripts/message/MessageForm.js
+++ src/scripts/message/MessageForm.js
@@ -1,5 +1,63 @@
-const messageForm =  () => {
-    let html = /*html*/ `
-    Message<br><br><input type="text" placeholder="Message to user"><br>
-    <input type="Button" value="Save" name="save"><input type="Button" value="Cancel" name="cancel">`
+import { getUsers } from "./provider.js"
+
+const userList = getUsers()
+
+let displayMessageForm = false
+
+const renderBlankMessageForm = () => {
+    return ""
 }
+
+const renderFullMessageForm =  () => {
+    return /*html*/ `
+    <article class="message-form">
+    <div><h4>Direct Message</h4>
+    <input type="button" id="close" value="x">
+    </div>
+
+    <div>Recipient:</div>
+    <div>
+    <select>
+    <option>Choose a recipient...</option>
+    ${userList.map(
+        (user) => {
+            return `<option value="user.id">${user.name}</option>`
+        }
+    )}
+    </select>
+    </div>
+
+    <div>Message</div>
+    <div>
+    <input type="text" placeholder="Message to user">
+    </div>
+    
+    <div>
+    <input type="Button" value="Save" name="save"><input type="Button" value="Cancel" name="cancel">
+    </div>
+    </article>`
+}
+
+const renderMessageForm = () => {
+    if(displayMessageForm === true) {
+        renderBlankMessageForm()
+    } else {
+        renderFullMessageForm()
+    }
+}
+
+document.addEventListener("click",
+    (evt) => {
+        if(evt.target.id === "message-form-button")
+        displayMessageForm = true
+        renderMessageForm()
+    }
+)
+ 
+document.addEventListener("click",
+    (evt) => {
+        if(evt.target.id === "close")
+        displayMessageForm = false
+        renderMessageForm()
+    }
+ )
--- src/scripts/nav/NavBar.js
+++ src/scripts/nav/NavBar.js
@@ -1,3 +1,4 @@
 export const navbar = () => {
-    return `<h2>NavBar</h2>`
+    return `<h2>NavBar</h2>
+    <div id="message-form-button>Messages</div>`
 }
