f33a068d175c2a79321d88f0375a213700f49222
Noah Husain <noahhusain@gmail.com>
2021-05-21 09:23:41 -0500

    Posts are now rendering on the page

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -8,7 +8,12 @@ export const giffyFeed = () => {
     return `
     <h1>Giffy Feed</h1>
     ${posts.map(post => 
-        `${post.title} ${post.URL} ${post.description}`
+        `<section class='post'><h2 class="post__title">${post.title}</h2> 
+        <img class="post__image" src='${post.URL}' 
+        <div class='post__description'>${post.description}</div>
+        <div class="post__tagline">Posted by insert user Id logic here</div>
+        <div class="post__actions"></div>
+        </section>`
     )}
     `
 }
