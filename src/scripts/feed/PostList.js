b5bbcfd2869c2e1a7805ccc18b56c2be8f8abe6a
Noah Husain <noahhusain@gmail.com>
2021-05-24 10:56:21 -0500

    Added the 2 action icons from giffygram. Working on making the delete post icon function like the button did.

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -14,7 +14,15 @@ export const giffyFeed = () => {
         <img class="post__image" src='${post.URL}'</img> 
         <div class='post__description'>${post.description}</div>
         <div class="post__tagline">Posted by ${creator.name} on ${post.timestamp}</div>
-        <div class="post__actions"></div>
+        <div class="post__actions">
+        <div>
+            <img id="${post.id}" class="actionIcon" src="/images/favorite-star-yellow.svg">
+                </div>
+        <div>
+        <img id="${post.id}" class="actionIcon" src="/images/block.svg">
+            </div>
+
+        </div>
         <button class="deletePost" id="deletePost" value='${post.id}'>Delete</button>
         </section>`
         }).join('')}
